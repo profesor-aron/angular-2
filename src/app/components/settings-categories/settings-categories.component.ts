82e718f8b11d4c3f939aabe311100194f2f447a8
Aron Herrera <aronherrera2009@gmail.com>
2016-08-24 23:00:27 +0200

    To develop the view "Settings of Categories".

--- src/app/components/settings-categories/settings-categories.component.ts
+++ src/app/components/settings-categories/settings-categories.component.ts
@@ -1,16 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../shared/services/data/data.service';
+import { FaDirective } from 'angular2-fontawesome/directives';
+import { Router } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'app-settings-categories',
   templateUrl: 'settings-categories.component.html',
-  styleUrls: ['settings-categories.component.css']
+  styleUrls: [
+    'settings-categories.component.css',
+    '../../../vendor/angular2-fontawesome/node_modules/font-awesome/css/font-awesome.css'
+  ]
 })
 export class SettingsCategoriesComponent implements OnInit {
 
-  constructor() { }
+  private model;
+
+  private catsCits = null;
+
+  constructor(
+    private dataService: DataService,
+    private router: Router) { }
 
   ngOnInit() {
+
+    this.initModel();
+
+  }
+
+  initModel() {
+
+    this.dataService.data.subscribe(data => {
+
+      this.catsCits = data["catsCits"];
+
+      this.initModelSubscribe();
+
+    });
+
+  }
+
+  initModelSubscribe() {
+
+  }
+
+  goToCategories() {
+
+    this.router.navigate(['categories']);
+
   }
 
 }
